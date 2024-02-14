54fc8548428040c43b587554ba045cebb754203f
Tony <tony.guan@etu.u-paris.fr>
2024-02-14 11:42:05 +0100

    Etape 2

--- R6.A.05-TP-Secu1-main/src/server.js
+++ R6.A.05-TP-Secu1-main/src/server.js
@@ -1,41 +1,38 @@
-import Fastify from "fastify"
-import fastifyBasicAuth from "@fastify/basic-auth"
-import fs from "fs"
-
+import Fastify from "fastify";
+import fastifyBasicAuth from "@fastify/basic-auth";
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const port = 3000;
-const authenticate = {realm: 'Westeros'}
-const path = "E:/Cours/TP/TP4/R6A05TP4/R6A05TP4/R6.A.05-TP-Secu1-main/src/https"
+const authenticate = {realm: 'Westeros'};
+
+// Convertir import.meta.url en chemin de dossier
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const fastify = Fastify({
-    logger: true
-})
-
-
-const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'https', 'fastify.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'https', 'fastify.cert'))
-}
-
-
-fastify.register(fastifyHttps, {
-    httpPort: 3000
-})
+    logger: true,
+    https: {
+        key: await fs.readFile(join(__dirname, 'server.key')),
+        cert: await fs.readFile(join(__dirname, 'server.crt'))
+    }
+});
 
 fastify.register(fastifyBasicAuth, {
     validate,
     authenticate
-})
+});
 
 async function validate(username, password, req, reply) {
     if (username !== 'Tyrion' || password !== 'wine') {
-        return new Error('Winter is coming')
+        return new Error('Winter is coming');
     }
 }
 
 fastify.get('/dmz', {}, (req, res) => {
-    res.send({replique: "Ca pourrait être mieux protégé..."})
-})
+    res.send({replique: "Ca pourrait être mieux protégé..."});
+});
 
 fastify.after(() => {
     fastify.route({
@@ -45,34 +42,32 @@ fastify.after(() => {
         handler: async (req, reply) => {
             return {
                 replique: 'Un Lannister paye toujours ses dettes !'
-            }
+            };
         }
-    })
+    });
     fastify.route({
         method: 'GET',
         url: '/autre',
         handler : async (req, reply) => {
-            return{
+            return {
                 replique : 'Route sans mdp ouvert à tous !'
-            }
+            };
         }
-        })
-})
+    });
+});
 
 fastify.setErrorHandler(function (err, req, reply) {
-
     if (err.statusCode === 401) {
-        console.log(err)
-        reply.code(401).send({replique: 'Tu ne sais rien, John Snow..'})
+        console.log(err);
+        reply.code(401).send({replique: 'Tu ne sais rien, John Snow..'});
     }
-    reply.send(err)
-})
+    reply.send(err);
+});
 
 fastify.listen({port}, function (err, address) {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
     }
-
-    fastify.log.info(`Fastify is listening on port: ${address}`);
+    fastify.log.info(`Server listening on ${address}`);
 });
